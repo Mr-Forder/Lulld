44a3319e3582360a9cb968fdc45e7c6202921d7d
Mr-Forder <66869833+Mr-Forder@users.noreply.github.com>
2021-08-16 23:24:31 +0100

    2g-added track controller to song component

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,4 +1,19 @@
-const Song = ({ currentSong, isPlaying }) => {
+const Song = ({ currentSong, isPlaying, audioRef, songInfo, setSongInfo }) => {
+  const getTime = (time) => {
+    return (
+      Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+    );
+  };
+  const dragHandler = (e) => {
+    audioRef.current.currentTime = e.target.value;
+    setSongInfo({ ...songInfo, currentTime: e.target.value });
+  };
+
+  const trackAnim = {
+    transform: `translateX(${songInfo.animationPercentage}%)`, //interpolate animation percentege value into css translate so will animate on the fly
+    //We then set the style of our animate-style div to interpolated value of this variable (on line)
+  };
+
   return (
     <div className="song-container">
       <div className="song-img">
@@ -12,6 +27,26 @@ const Song = ({ currentSong, isPlaying }) => {
         <h4>Now Playing:</h4>
         <h2>{currentSong.name}</h2>
         <h3>{currentSong.artist}</h3>
+      </div>
+      <div className="time-control">
+        <p>{getTime(songInfo.currentTime)}</p>
+
+        <div
+          className="track"
+          style={{
+            background: `linear-gradient(to right, ${currentSong.color[0]},${currentSong.color[1]})`,
+          }}
+        >
+          <input
+            min={0}
+            max={songInfo.duration || 0}
+            value={songInfo.currentTime}
+            onChange={dragHandler}
+            type="range"
+          />
+          <div style={trackAnim} className="animate-track"></div>
+        </div>
+        <p>{getTime(songInfo.duration || 0)}</p>
       </div>
     </div>
   );
